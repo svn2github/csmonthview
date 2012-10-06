02c69b5b9f2e1edbd02e4861b8a12bca380d4ea9
tomac120@gmail.com <tomac120@gmail.com@2338ade3-34c9-13e7-2a7a-eec767111304>
2012-10-06 02:41:55 +0000

    a
    git-svn-id: http://csmonthview.googlecode.com/svn@3 2338ade3-34c9-13e7-2a7a-eec767111304

---
+++ trunk/CSMonthView DEMO/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  CSMonthView DEMO
+//
+//  Created by Elijah on 10/5/12.
+//  Copyright (c) 2012 Elijah. All rights reserved.
+//  Free for personal and commercial use. (GNU Lesser GPL - any version)
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
